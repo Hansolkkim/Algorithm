b8a1cd7e463188bdb5e6c2a5216c295d1ef05474
sol <hskim6521@naver.com>
2021-12-04 15:15:23 +0900

    add solution of "크레인 인형뽑기 게임" and update README.md

---
+++ Programmers/Programmers/크레인 인형뽑기 게임.swift
@@ -0,0 +1,36 @@
+//
+//  크레인 인형뽑기 게임.swift
+//  Programmers
+//
+//  Created by 김한솔 on 2021/12/04.
+//
+
+import Foundation
+
+func clawMachine (_ board: [[Int]], _ moves: [Int]) -> Int {
+    var currentBoard = board
+    var countPopedDoll: Int = 0
+    var basketStack = [Int]()
+    for column in moves {
+        let realColumn = column - 1
+        for row in 0..<board[0].count {
+            if currentBoard[row][realColumn] != 0 {
+                if basketStack.isEmpty { //바구니가 비어있었다면 그냥 뽑은 인형을 담으면 됨
+                    basketStack.append(currentBoard[row][realColumn])
+                } else { //바구니가 비어있지 않았다면, 바구니 맨 위에 있는 인형과 뽑은 인형을 비교 후 넣을지 말지 해야함
+                    let popedDoll: Int! = basketStack.popLast()
+
+                    if popedDoll != currentBoard[row][realColumn] { // 바구니 맨 위에 있던 인형과 뽑은 인형이 다르면 그냥 담으면 됨.
+                        basketStack.append(popedDoll)
+                        basketStack.append(currentBoard[row][realColumn])
+                    } else {
+                        countPopedDoll += 2
+                    }
+                }
+                currentBoard[row][realColumn] = 0
+                break
+            }
+        }
+    }
+    return countPopedDoll
+}
--- Programmers/README.md
+++ Programmers/README.md
@@ -1,3 +1,4 @@
 #  프로그래머스 알고리즘 해결 목록
 
 [나누어 떨어지는 숫자 배열](https://programmers.co.kr/learn/courses/30/lessons/12910)
+[크레인 인형뽑기 게임](https://programmers.co.kr/learn/courses/30/lessons/64061)
