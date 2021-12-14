3422f2838bd2de2bcd425c7a68d6b957bec33e3a
sol <hskim6521@naver.com>
2021-12-14 15:15:18 +0900

    Modify "가운데 글자 가져오기"'s solution using String(Array(s))

--- Programmers/Programmers/가운데 글자 가져오기.swift
+++ Programmers/Programmers/가운데 글자 가져오기.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 func pickMiddleChar(_ s: String) -> String {
-    return s.count%2 != 0 ? String(s[s.index(s.startIndex, offsetBy: (s.count-1)/2)]) : String(s[s.index(s.startIndex, offsetBy: (s.count/2)-1)...s.index(s.startIndex, offsetBy:s.count/2)])
+    return s.count%2 != 0 ? String(Array(s)[(s.count-1)/2]) : String(Array(s)[(s.count/2)-1...s.count/2])
 }
