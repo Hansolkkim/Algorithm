c1d3989f05df5a2158fc3dd3c6d269000d42663f
sol <hskim6521@naver.com>
2021-12-16 10:41:56 +0900

    Add another solution of  "로또 최고 순위와 최저 순위" using filter

--- Programmers/Programmers/로또의 최고 순위와 최저 순위.swift
+++ Programmers/Programmers/로또의 최고 순위와 최저 순위.swift
@@ -8,21 +8,17 @@
 import Foundation
 
 func highestAndLowestRankInLotto(_ lottos: [Int],_ win_nums: [Int]) -> [Int] {
-    var matched = 0, zeros = 0
-    for lotto in lottos {
-        if win_nums.contains(lotto) {matched += 1}
-        if lotto == 0 {zeros += 1}
-    }
-    var rank: [Int] = [matched + zeros, matched]
+    let zeroCount = lottos.filter{ $0 == 0 }.count
+    let matchedCount = win_nums.filter {lottos.contains($0)}.count
+    let rank: [Int] = [matchedCount + zeroCount, matchedCount]
     
     return rank.map {switch $0 {
-    case ...1: return 6
     case 2: return 5
     case 3: return 4
     case 4: return 3
     case 5: return 2
     case 6: return 1
-    default: return 0
+    default: return 6
     }
         
     }
