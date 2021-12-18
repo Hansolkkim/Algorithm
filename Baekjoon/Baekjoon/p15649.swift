3f3c74f9f5ae651f15d9f5c3b18f3d66fb3b69d4
sol <hskim6521@naver.com>
2021-12-18 13:30:38 +0900

    Refactor the solution of p15649
    Refactor "permutation()" function to make have return type

--- Baekjoon/Baekjoon/p15649.swift
+++ Baekjoon/Baekjoon/p15649.swift
@@ -8,23 +8,23 @@
 import Foundation
 
 func nAndM1() {
-    func permutation(_ a : [Int], _ n: Int,_ idx: Int = 0) {
+    func permutation(_ a : [Int], _ n: Int,_ idx: Int = 0) -> [[Int]] {
+        var returnArray = [[Int]]()
         if idx == n {
-            permutationNumberArray.append([Int](a[0..<idx]))
-            return
+            returnArray.append([Int](a[0..<idx]))
         } else {
             var a = a
             for i in idx..<a.count {
                 a.swapAt(idx, i)
-                permutation(a, n, idx+1)
+                returnArray += permutation(a, n, idx+1)
             }
         }
+        return returnArray
     }
     
     let inputFromUser = readLine()?.components(separatedBy: .whitespacesAndNewlines).map{Int($0)!}
-    var permutationNumberArray = [[Int]]()
+    let permutationNumberArray = permutation([Int](1...inputFromUser![0]), inputFromUser![1])
     
-    permutation([Int](1...inputFromUser![0]), inputFromUser![1])
     permutationNumberArray.forEach {
         var tempString = ""
         for i in 0..<$0.count {
